ed3227d096f491d23f92a96bd54006aa3647585f
Bunyod <b.eshkuvvatov@gmail.com>
2026-03-15 21:10:25 +0300

    полностью переписал логику start, но пока она находится в main, нужно написать start с подобранными Q

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-
 # ruff: noqa: F403, F405
 import Data as dt
 from math import exp
 import functions as fc
 import saor
-import start
-T0 = start.T[:dt.n_1 + dt.n_az + dt.n_2] + start.T[dt.n_1 + dt.n_az:dt.n_1 + dt.n_az + dt.n_2] * 2 + [0] * 51
+T0 = [0] * (dt.N + 1)
 h = 2.25
-dtime = 500
+dtime = 300
 time = 0.05
 n = 0
-
 # Цикл естественной циркуляции
 # 1.Активная зона  2.Область до тягового участка  3. Тяговый участок до отметки естественной циркуляции
 # 4. Горизонтальный участок до САОР  5.Теплообменник Фильда  6. Опускной участок 7. Горизонтальный участок до АЗ 
-T1 = [623,15] + [0] * dt.N
+T1 = [623.15] + [0] * dt.N
 def part_x(n, f, h, G):
     global i, T1
     dx = h / n
@@ -30,7 +27,7 @@ def part_x(n, f, h, G):
 while time < dtime:
     # Активная зона
     G = dt.Gpb
-    Q_veg = 0.065 * dt.Q * (time ** (-0.2) - (time + 2592000) ** (-0.2))
+    Q_veg = 0.065 * dt.Q
     i = 1
     dx = dt.h_az / dt.n_az
     dt_dx = dt.dt / dx
@@ -51,8 +48,8 @@ while time < dtime:
     # Горизонтальный участок до САОР
     part_x(dt.n_3, dt.f_3, dt.l_3, G)
     # CАОР
-    t_saor = saor.saor_calc(T1[-1], G/12)
-    for j in range(1, len(t_saor)):
+    t_saor = saor.saor_calc(T1[i-1], G/12)
+    for j in range(len(t_saor)):
         T1[i] = t_saor[j]
         i += 1
     # Опускной участок
@@ -63,10 +60,9 @@ while time < dtime:
     
     time += dt.dt
     T0 = T1[:]
-    T1 = [T1[-1]] + [0] * dt.N
+    T1 = [623.15] + [0] * dt.N
+
 print(T0)
-
-
 
 
 
--- start.py
+++ start.py
@@ -1,68 +1,27 @@
-import start_data as st
+# TODO 1 переписать программу start, сделав его стационарным расчетом всего контура с заданной температурой входа
+# таким образом получить распределения температур по всему контуру в первом приближении
+import Data as dt
 import functions as fc
+import saor
 
-h = 2.25
-T_k = [0] * (st.N + 1)
-dtime = 250
-time = 0
-n = 0
-T_k_1 = [st.T0] + [0] * st.N
-def part(n, f, h):
-    global T_k_1, i
-    dx = h / n
-    dt_dx = st.dt / dx
-    for j in range(n):
-        t_i = T_k[i]  # T_i-1_k
-        t_i_1 = T_k[i-1]  # T_i_k
-        r = fc.ro(t_i_1 - 273.15)
-        t_k_1 = round(t_i + dt_dx * (st.Gpb* st.cp_Pb*(t_i_1 - t_i)) / (st.cp_Pb * r * f), 3)
-        T_k_1[i] = t_k_1
-        i += 1
+G = 500
+T = [350 + 273.5]
+t_cur = 350 + 273.5
+Q = 0.065 * dt.Q
+z = 0
+dx = dt.h_az / dt.n_az
+for j in range(dt.n_az):
+    t_temp = t_cur + fc.ql(Q, z) * dx / (G * dt.cp_Pb)
+    T.append(t_temp)
+    t_cur = t_temp
+    z += dx
 
-while time < dtime:
-    # Активная зона
-    i = 1
-    dx = st.h_az / st.n_az
-    dt_dx = st.dt / dx
-    h = 2.25
-    z = 0
-    for j in range(st.n_az):
-        t_i = T_k[i]  # T_i-1_k
-        t_i_1 = T_k[i-1]  # T_i_k
-        r = fc.ro(t_i_1 - 273.15)
-        t_k_1 = round(t_i + dt_dx * (st.Gpb* st.cp_Pb*(t_i_1 - t_i) + fc.ql(st.Q, z) * dx) / (st.cp_Pb * r * st.f_az), 3)
-        T_k_1[i] = t_k_1
-        h += dx
-        i += 1
-        z += dx
-    # Область до тягового участка
-    part(st.n_1, st.f_1, st.h_1)
-    # Тяговый участок
-    part(st.n_2, st.f_2, st.h_2)
-    # Парогенератор
-    h = 7.75
-    dx = st.h_pg / st.n_pg
-    dt_dx = st.dt / dx
-    for j in range(st.n_pg):
-        t_i = T_k[i]  # T_i-1_k
-        t_i_1 = T_k[i - 1]  # T_i_k
-        r = fc.ro(t_i_1 - 273.15)
-        alfa = fc.alphaPb(r, st.f_pg, st.dg_pg, st.Gpb)
-        t_k_1 = round(t_i + dt_dx * (st.Gpb * st.cp_Pb * (t_i_1 - t_i) + alfa * st.s_pg * (st.T_pg - t_i)) / (r * st.f_pg * st.cp_Pb), 3)
-        T_k_1[i] = t_k_1
-        h -= dx
-        i += 1
-    # Вертикальный участок с ГЦН
-    part(st.n_3, st.f_3, st.h_3)
-    # Опускной участок
-    part(st.n_4, st.f_4, st.h_4)
-    # Горизонтальный участок
-    part(st.n_5, st.f_5, st.l_5)
-    # Подъемный участок до активной зоны
-    part(st.n_6, st.f_6, st.h_6)
+T += [T[-1]] * 20
     
-    time += st.dt
-    T_k = T_k_1
-    T_k_1 = [st.T0] + [0] * st.N
+t_saor = saor.saor_calc(T[-1], G / 12)
+T += t_saor
+T += [T[-1]] * 15
+print(T)
     
-T = T_k
+        
+
